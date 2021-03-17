7ed00131594113851dcd57db03cd9fc0501ae674
Andrey Valent <hhdusha@gmail.com>
2021-03-17 19:31:06 +0300

    modified:   ch_select2.sh

--- history/ch_select2.sh
+++ history/ch_select2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-clickhouse-client --port="9000" --query="select * from default.metrics_store"
+clickhouse-client --port="9000" --host=clickhouse-omsk.rmsi.srv.loc\
+ --query="select * from default.metrics_store limit 100"
 #
 #clickhouse-client --host=clickhouse-omsk.rmsi.srv.loc\
 # --user=admin --password=oxoeWie5 --query "select * from default.metrics_store" --format CSVWithNames > metrics_store.csv
